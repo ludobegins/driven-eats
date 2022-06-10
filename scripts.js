4335528502ea3b592c2923fe9d4329f48967c29a
ludobegins <ludo.beghin@gmail.com>
2022-06-10 10:51:38 -0300

    seleciona 1 e somente 1 em cada categoria

--- scripts.js
+++ scripts.js
@@ -1,6 +1,16 @@
-
 function toggleSelection(el){
-    console.log(el);
     el.classList.toggle('selected');
+    
+    //classList[1] = food OU drink OU dessert
+    // PERCORRER TODOS. SE CLASSES[1] = CLASSES[1] DO ELEMENTO CLICADO -> REMOVER SELECTED
+    
     let food_items = document.querySelectorAll('.food-item');
+    for(let i=0; i< food_items.length; i++){
+        if (food_items[i] !== el){
+            if (food_items[i].classList[1] === el.classList[1]){
+                food_items[i].classList.remove('selected');
+            }
+        }
+    }
+
 }
